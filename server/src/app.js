2aff38fb6aee293cc8d7a5460bf48df15966d519
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2023-02-04 01:00:41 +0200

    Logs with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require("express");
 const cors = require('cors');
-
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +10,7 @@ const whitelist = {
 }
 const app = express();
 app.use(cors(whitelist)); // if left emtpy it will accept all
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname , '..' , 'public'))); //a middleware that serves all our public files
 app.use(planetsRouter);
