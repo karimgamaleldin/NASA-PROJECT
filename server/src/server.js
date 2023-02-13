2681afb2f93b5e72ad40886f573dd92416085341
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2023-02-13 14:08:46 +0200

    Clusters using PM2

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const cluster = require('cluster');
 const {loadPlanetsData} = require('./models/planets.models');
 
 const PORT = process.env.PORT || 8000;
